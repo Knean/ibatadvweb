fdffe13751aadeff1bed1b970f443a2b2c2f0615
John rowley <jrowley@student.ibat>
2018-11-21 19:47:08 +0000

    Added local storage

---
+++ octdec2018/lesson8/js/site.js
@@ -0,0 +1,83 @@
+$(function () {
+
+    console.log("Jquery goodness is loaded");
+    /*
+        localStorage.setItem("car-1", "Ford1");
+        localStorage.setItem("car-2", "Ford2");
+        localStorage.setItem("car-3", "Ford3");
+        localStorage.setItem("car-4", "Ford4");
+    */
+    loadFlavour(); // Load the flavour saved in previous session
+
+    //Set up listener to save to local storage on button click 
+
+    $('#btnSave').on('click', function () {
+        saveFlavour(); // This function is defined outside the jquery ready function
+    })
+
+    $('#btnSaveCar').on('click', function () {
+        saveCar(); // This function is defined outside the jquery ready function
+    })
+
+
+})
+
+/*
+    Loads flavour from local storage
+*/
+function loadFlavour() {
+    console.log("loading flavour")
+    var savedFlavour = localStorage.getItem('favFlavour');
+
+    $('#savedFlavour').val(savedFlavour)
+
+}
+/*
+    Saves flavour from local storage
+*/
+function saveFlavour() {
+    console.log("Saving flavour")
+    localStorage.setItem('favFlavour', $('#favFlavour').val());
+}
+
+
+function saveCar() {
+    var car = {};
+    car.wheels = 4;
+    car.doors = 2;
+    car.sound = 'vroom';
+    car.name = 'Lightning McQueen';
+    console.log( car );
+    localStorage.setItem( 'car', JSON.stringify(car) );
+    console.log( JSON.parse( localStorage.getItem( 'car' ) ) );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _saveCar() {
+    var car = {};
+    car.wheels = 4;
+    car.doors = 2;
+    car.sound = 'vroom';
+    car.name = 'Lightning McQueen';
+    console.log( car );
+    localStorage.setItem( 'car', JSON.stringify(car) );
+    console.log( JSON.parse( localStorage.getItem( 'car' ) ) );
+
+}
