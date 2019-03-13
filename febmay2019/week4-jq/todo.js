37f3e68af4531dd6724b2f112d3a805d8c572518
John rowley <jrowley@student.ibat>
2019-03-13 21:01:45 +0000

    Added checkboxes and ternary operator

--- febmay2019/week4-jq/todo.js
+++ febmay2019/week4-jq/todo.js
@@ -1,31 +1,44 @@
 
-var toDoItems=["bins","shops","washing"];
+var toDoItems = [
+    { title: "bins", completed: true },
+    { title: "shops", completed: false },
+    { title: "washing", completed: false }];
 
-$(function() {
+$(function () {
     console.log("to do is loaded");
-    initButtons() ;
+    initButtons();
     renderToDoList(toDoItems);
+
 
 })
 
+function styleList() {
+
+    $("#todoList li:eq( 2 )").css("color", "red");
+}
+
 function initButtons() {
     $('#btnAddItem').on("click", function () {
-           toDoItems.push( $('#tbItem').val() );
-           renderToDoList(toDoItems);
+
+        var todo = { title: $('#tbItem').val(), completed: false }
+        toDoItems.push(todo);
+        renderToDoList(toDoItems);
     });
 }
 
 function renderToDoList(listOfItems) {
 
-    var htmlString =[];
-    htmlString.push("<ol>");
+    var htmlString = [];
+    htmlString.push("<ol id='todoList'>");
 
-    var lineItems = listOfItems.map( currentItem => {
-
-        return `<li>${currentItem}</li>`
+    var lineItems = listOfItems.map(currentItem => {
+        //ternary operator  var stuff = condition ? true: false; 
+        var isCompleted = currentItem.completed ? ' checked ' : '';
+        var cb = `<input type='checkbox' ${isCompleted}/>`;
+        return `<li>${cb} ${currentItem.title}</li>`
 
     })
-    console.log({lineItems});
+    console.log({ lineItems });
     htmlString.push(lineItems.join(" "));
 
     /*
@@ -39,5 +52,7 @@ function renderToDoList(listOfItems) {
     */
     htmlString.push("</ol>");
 
-    $('#messagePanel').html( htmlString.join(" "));
+    $('#messagePanel').html(htmlString.join(" "));
+
+    styleList();
 }
